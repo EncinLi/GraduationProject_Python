518e7f55d16a43ae622668bb8b78df04e34f7093
Encin.Lee <Encin_Li@163.com>
2019-03-07 22:23:46 +0800

    Start Build Project --Encin.Li

---
+++ graduation_project/spiders/economic_data_spider.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class EconomicDataSpiderSpider(CrawlSpider):
+    name = 'economic_data_spider'
+    allowed_domains = ['www.jiangmen.gov.cn/szdwzt/jmtjj/']
+    start_urls = ['http://www.jiangmen.gov.cn/szdwzt/jmtjj//']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        return item
---
+++ start_crawl.py
@@ -0,0 +1,3 @@
+from scrapy import cmdline
+
+cmdline.execute("scrapy crawl economic_data_spider".split())
